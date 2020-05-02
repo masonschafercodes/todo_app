a263e0eecbdc289912d1d7cdebbcf8ac4896e0d1
Mason Schafer <57614646+masonschafercodes@users.noreply.github.com>
2020-05-02 01:23:05 -0400

    Uploaded Files

---
+++ todo_list_app/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+//Create a way to store items
+//Features:
+//1. Adding Items
+//2. Completing Items
+//3. Remove Items
+//------------------
+//How to solve:
+//1. Create a list and append each Item to that list
+//2&3. Remove item from list
+
+namespace todo_list_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Todo new_list = (Todo)CreateNewTodo(); //Creating a new Todo object
+            string date = DateTime.Now.ToShortDateString(); //Setting up date string so I can display date on main menu
+            string time = DateTime.Now.ToString("hh:mm:ss tt"); //Setting up time string so I can display time on main menu
+            bool keep_running = true;
+            while (keep_running) //Main loop for main menu
+            {
+                Console.WriteLine("##########################");
+                Console.WriteLine("#    Date: " +  date + "      #");
+                Console.WriteLine("# Created At: " +  time + "#");
+                Console.WriteLine("##########################");
+                Console.WriteLine("#        Todo List       #");
+                Console.WriteLine("##########################");
+                Console.WriteLine("#    1: Add Item         #");         //MAIN MENU
+                Console.WriteLine("#    2: Remove Item      #");
+                Console.WriteLine("#    3: Export List      #");
+                Console.WriteLine("#    4: Display Items    #");
+                Console.WriteLine("#    5: Exit             #");
+                Console.WriteLine("##########################");
+                Console.Write("> ");
+                string user_option = Console.ReadLine();  //Capturing User Input
+
+                if (user_option == "1")
+                {
+                    new_list.Todo_Items(); //Running class method to add items to a list
+                    Console.Clear();
+                } else if (user_option == "2")
+                {
+                    for (int i = 1; i <= new_list.items.Count; i++) //Looping through list items so user knows what they are deleting 
+                    {
+                        Console.WriteLine("(" + i + ")" + "- " + new_list.items[i - 1]);
+                    }
+                    new_list.DeleteItems(); //Running class method to delete items to a list
+                    Console.Clear();
+                } else if (user_option == "3")
+                {
+                    try
+                    {
+                        if (new_list.items.Count != 0)
+                        {
+                            StreamWriter sw = new StreamWriter("ENTER PATH TO FOLDER TO EXPORT" + new_list.title + ".txt");
+                            sw.WriteLine("List Name: " + new_list.title);
+                            for (int i = 1; i <= new_list.items.Count; i++)
+                            {
+                                sw.WriteLine("- " + new_list.items[i - 1]);
+
+                            }
+                            sw.Close();
+                            Console.WriteLine("Successfully saved document."); //This block of code is for exporting current list to a folder for later user
+                            Console.ReadLine();
+                            Console.Clear();
+                        } else
+                        {
+                            Console.WriteLine("Please Add an Item to the List");
+                            Console.ReadLine();
+                            Console.Clear(); 
+                        }
+
+                    } catch
+                    {
+                        Console.WriteLine("Unable to Write to file.");
+                        Console.ReadLine();
+                    }
+                }
+                else if (user_option == "4")
+                {
+                    Console.Clear();
+                    for (int i = 1; i <= new_list.items.Count; i++)
+                    {
+                        Console.WriteLine("(" + i + ")" + "- " + new_list.items[i - 1]);   //Looping through list items to display to the user
+                    }
+                    Console.Write("Press Enter to Return to Main Menu...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (user_option == "5")
+                {
+                    Environment.Exit(0); //Exiting the  application when user picks "Exit"
+                }
+                else
+                {
+                    Console.WriteLine("Unknown Input Please Try Again."); //Catching error on input
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+
+        public static Object CreateNewTodo() //Method to create initial object and run constructor
+        {
+            Console.Write("Title for your todo list: ");
+            string titleForTodo = Console.ReadLine();
+
+            return new Todo(titleForTodo);
+
+        }
+        
+    }
+}
---
+++ todo_list_app/Todo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+namespace todo_list_app
+{
+    public class Todo //Class for creating a new list with specefic methods for adding and deleting items from that list
+    {
+        public string title;
+        public int numberOfItemsOnList;
+        public List<string> items = new List<string>();
+
+        public Todo(string aTitle)
+        {
+            title = aTitle;
+        }
+
+        public List<string> Todo_Items()
+        {
+            Console.Write("Add an item to the list: ");
+            string new_item = Console.ReadLine();
+            if (new_item != "")
+            {
+                items.Add(new_item);
+            } else
+            {
+                Console.WriteLine("Please enter an item to add to the list");
+                Console.ReadLine();
+            }
+
+            return items;
+        }
+
+        public List<string> DeleteItems()
+        {
+            Console.Write("Which Item to Delete: ");
+            try
+            {
+                int item_to_delete = Convert.ToInt32(Console.ReadLine());
+                items.Remove(items[item_to_delete - 1]);
+            }
+            catch
+            {
+                Console.WriteLine("Unable to Parse that Number");
+            }
+            return items;
+        }
+    }
+}
